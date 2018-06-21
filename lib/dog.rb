818fc972a8949498bed078e5a77220d9a7750251
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-06-21 12:42:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,14 @@
 class Dog
+  attr_accessor :name
   @@all = []
+
   def initialize(name)
     @name = name
-  end 
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
 end
